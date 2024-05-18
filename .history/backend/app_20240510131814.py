e5fe76fd19a3614b8b0fc8d993181cc2d45c89b0
Fechinmitchell <fechinmitchell1996@gmail.com>
2024-05-18 19:44:32 +1000

    Added loading screen to Players component and updated CSS

---
+++ .history/backend/app_20240510131814.py
@@ -0,0 +1,127 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import firebase_admin
+from firebase_admin import credentials, storage
+import datetime
+import pandas as pd
+
+# Initialize Firebase Admin
+cred_path = '/Users/fechinmitchell/Projects/GAAStudio/backend/gaastudio-2a7ac-firebase-adminsdk-pxkjd-a10063e0b0.json'
+cred = credentials.Certificate(cred_path)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'gaastudio-2a7ac.appspot.com'
+})
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/players', methods=['GET'])
+def get_player_stats():
+    try:
+        # You need to specify the correct path to your JSON file in Firebase Storage
+        path = 'backend/Football_Championship_2023_Shots_With_Expected_Points.json.json'
+        url = generate_signed_url(path)
+        # Fetching data as JSON directly, assuming it's stored in a format that can be directly converted to JSON
+        response = pd.read_json(url)
+        # Convert DataFrame to JSON
+        player_stats_json = response.to_dict(orient='records')
+        return jsonify(player_stats_json)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+# Function to generate signed URLs for Firebase Storage objects
+def generate_signed_url(blob_path):
+    bucket = storage.bucket()
+    blob = bucket.blob(blob_path)
+    return blob.generate_signed_url(expiration=datetime.timedelta(hours=1), method='GET')
+
+# Define endpoints for serving various types of maps and data
+@app.route('/pressuremaps/<team>')
+def get_pressure_map(team):
+    path = f"datasets/Pressure/{team}_Pressure_Map.png"
+    return jsonify({'url': generate_signed_url(path)})
+
+@app.route('/tacklesmaps/<team>')
+def get_tackle_map(team):
+    path = f"datasets/Tackles/{team}_unsuccesful_tackles.png"
+    return jsonify({'url': generate_signed_url(path)})
+
+@app.route('/blockedmaps/<team>')
+def get_blocked_map(team):
+    path = f"datasets/Blocked/{team}_block_points.png"
+    return jsonify({'url': generate_signed_url(path)})
+
+@app.route('/sdpmchart/<team>')
+def get_sdpm_chart(team):
+    path = f"datasets/SDPM/{team}_SDPM_Chart.png"
+    return jsonify({'url': generate_signed_url(path)})
+
+@app.route('/asdmap/<team>')
+def get_asd_map(team):
+    path = f"datasets/ASD/{team}_average_shot_positions.png"
+    return jsonify({'url': generate_signed_url(path)})
+
+@app.route('/ppmaps/<team>')
+def get_pp_map(team):
+    path = f"datasets/PP/{team}_pressure_points.png"
+    return jsonify({'url': generate_signed_url(path)})
+
+@app.route('/heatmaps/<team>/<category>')
+def get_heatmap(team, category):
+    path = f"datasets/{category.capitalize()}/{team}_{category.lower()}_heat_map.jpg"
+    return jsonify({'url': generate_signed_url(path)})
+
+@app.route('/teams', methods=['GET'])
+def get_teams():
+    try:
+        # Fetch the dataset from Firebase Storage
+        df = get_dataframe_from_firebase('backend/Football_Championship_2023_Shots_With_Expected_Points.json')
+        # Extract unique team names
+        teams = df['TeamName'].unique().tolist()
+        return jsonify(teams)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+# Function to download file from Firebase Storage and convert to DataFrame
+def get_dataframe_from_firebase(blob_name):
+    url = generate_signed_url(blob_name)
+    return pd.read_json(url)
+
+# Define the scoring zone parameters
+GOAL_LINE = 145
+SCORING_ZONE_DEPTH = 32
+CENTER_WIDTH = 44
+SCORING_ZONE_HALF_WIDTH = 28
+
+# Function to check if a shot is within the scoring zone
+def is_within_scoring_zone(x, y):
+    return (GOAL_LINE - SCORING_ZONE_DEPTH) <= x <= GOAL_LINE and \
+           (CENTER_WIDTH - SCORING_ZONE_HALF_WIDTH) <= y <= (CENTER_WIDTH + SCORING_ZONE_HALF_WIDTH)
+
+
+# Endpoint for scoring zone efficiency using data from Firebase Storage
+@app.route('/scoring-zone-efficiency', methods=['GET'])
+def get_scoring_zone_efficiency():
+    team_name = request.args.get('team')
+    if not team_name:
+        return jsonify({'error': 'Team name is required'}), 400
+    try:
+        df = get_dataframe_from_firebase('backend/Football_Championship_2023_Shots_With_Expected_Points.json')
+        team_data = df[df['TeamName'] == team_name]
+        team_data['is_scoring_zone'] = team_data.apply(lambda row: is_within_scoring_zone(row['x'], row['y']), axis=1)
+        scoring_zone_counts = team_data.groupby('is_scoring_zone')['Score'].value_counts().unstack().fillna(0)
+        scoring_zone_counts['success_rate'] = scoring_zone_counts['Score'] / (scoring_zone_counts['Score'] + scoring_zone_counts.get('Miss', 0))
+        response_data = {
+            'inside_scoring_zone': scoring_zone_counts.loc[True].to_dict(),
+            'outside_scoring_zone': scoring_zone_counts.loc[False].to_dict()
+        }
+        return jsonify(response_data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
---
+++ .history/frontend/src/components/Players_20240518194056.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Spinner } from 'react-bootstrap';
+import './Players.css';
+
+function Players() {
+  const [playerStats, setPlayerStats] = useState([]);
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get('/players');
+        setPlayerStats(response.data);
+      } catch (error) {
+        console.error('Error fetching player stats:', error);
+      }
+      setTimeout(() => {
+        setLoading(false);
+      }, 500); // Simulate longer loading time
+    };
+    fetchData();
+  }, []);
+
+  const sortedPlayerStats = React.useMemo(() => {
+    let sortableItems = [...playerStats];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [playerStats, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  }
+  
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <Spinner animation="border" role="status" />
+        <span className="loading-text"></span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="players-container">
+      <h1 className="players-header">Players Stats</h1>
+      <div className="table-scroll-container">
+        <table className="players-table">
+          <thead className="players-thead">
+            <tr>
+              <th onClick={() => requestSort('PlayerName')}>Player Name</th>
+              <th onClick={() => requestSort('TeamName')}>Team Name</th>
+              <th onClick={() => requestSort('Scores')}>Scores</th>
+              <th onClick={() => requestSort('Shots')}>Shots</th>
+              <th onClick={() => requestSort('xP_adv_shot')}>xP Adv Shot</th>
+              <th onClick={() => requestSort('xPoints')}>xPoints</th>
+              <th onClick={() => requestSort('Success_UP')}>Success UP</th>
+              <th onClick={() => requestSort('Failed_UP')}>Failed UP</th>
+              <th onClick={() => requestSort('Scored_Right')}>Scored Right</th>
+              <th onClick={() => requestSort('Scored_Left')}>Scored Left</th>
+              <th onClick={() => requestSort('Scored_Hand')}>Scored Hand</th>
+              <th onClick={() => requestSort('Shot_Distance')}>Shot Distance</th>
+              <th onClick={() => requestSort('Difference')}>Difference</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedPlayerStats.map((player, index) => (
+              <tr key={index} className={`player-row ${index % 2 === 0 ? 'even' : 'odd'}`}>
+                <td>{player.PlayerName}</td>
+                <td>{player.TeamName}</td>
+                <td>{player.Scores}</td>
+                <td>{player.Shots}</td>
+                <td>{Number(player.xP_adv_shot).toFixed(2)}</td>
+                <td>{Number(player.xPoints).toFixed(2)}</td>
+                <td>{player.Success_UP}</td>
+                <td>{player.Failed_UP}</td>
+                <td>{player.Scored_Right}</td>
+                <td>{player.Scored_Left}</td>
+                <td>{player.Scored_Hand}</td>
+                <td>{Number(player.Shot_Distance).toFixed(2)}</td>
+                <td>{Number(player.Difference).toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Players;
